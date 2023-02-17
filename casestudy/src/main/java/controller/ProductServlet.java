167c2048c0ceff94be43c969a259b92462ef3f6c
ghgmdr <truongtq20@gmail.com>
2023-02-17 23:17:32 +0700

    truong

---
+++ casestudy/src/main/java/controller/ProductServlet.java
@@ -0,0 +1,92 @@
+package controller;
+
+import model.Product;
+import service.IProductService;
+import service.ProductService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet(name = "ProductServlet",urlPatterns = "/admin")
+public class ProductServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private IProductService productService;
+
+    public void init() {
+        productService = new ProductService();
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        try {
+            switch (action) {
+                case "create":
+                    insertUser(request, response);
+                    break;
+                case "edit":
+                    break;
+            }
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        try {
+            switch (action) {
+                case "create":
+                    showNewForm(request, response);
+                    break;
+                case "edit":
+                    break;
+                case "delete":
+                  int  id = Integer.parseInt(request.getParameter("deleteId"));
+                    productService.delete(id);
+                    response.sendRedirect("/student");
+                    break;
+
+                default:
+                    listUser(request, response);
+                    break;
+            }
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
+    }
+    private void listUser(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException, ServletException {
+        List<Product> listProduct = productService.selectAllUsers();
+        request.setAttribute("listProduct", listProduct);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("admin/list.jsp");
+        dispatcher.forward(request, response);
+    }
+    private void insertUser(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException, ServletException {
+        String tenSP = request.getParameter("tenSP");
+        String donGia = request.getParameter("donGia");
+        String soLuong = request.getParameter("soLuong");
+        String loaiSanPham = request.getParameter("loaiSanPham");
+        Product product = new Product(tenSP,donGia,soLuong,loaiSanPham);
+        productService.insertUser(product);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("admin/create.jsp");
+        dispatcher.forward(request, response);
+    }
+    private void showNewForm(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("admin/create.jsp");
+        dispatcher.forward(request, response);
+    }
+}
